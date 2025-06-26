6d0db0ad49bd4eb4bd476897128e218c7593c198
Dongho Kim <cha11enge@hanmail.net>
2025-06-26 23:15:00 +0900

    [level 0] Title: 이어 붙인 수, Time: 32.23 ms, Memory: 67 MB -BaekjoonHub

--- 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.kt
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.kt
@@ -1,13 +1,3 @@
 class Solution {
-    fun solution(num_list: IntArray): Int {
-        val v1 = StringBuilder()
-        val v2 = StringBuilder()
-        
-        num_list.forEach{
-            if(it % 2 == 0) v2.append(it)
-            else v1.append(it)
-        }   
-        
-        return v1.toString().toInt() + v2.toString().toInt()
-    }
+    fun solution(num_list: IntArray): Int = num_list.partition{ it % 2 == 0}.toList().sumOf{ it.joinToString("").toInt()}
 }
