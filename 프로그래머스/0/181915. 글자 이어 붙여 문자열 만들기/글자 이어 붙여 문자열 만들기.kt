cfc6047381fcf864ac61bd8dbe449b957f95021e
Dongho Kim <cha11enge@hanmail.net>
2025-06-27 12:23:45 +0900

    [level 0] Title: 글자 이어 붙여 문자열 만들기, Time: 17.66 ms, Memory: 63.8 MB -BaekjoonHub

--- 프로그래머스/0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.kt
+++ 프로그래머스/0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.kt
@@ -1,11 +1,5 @@
 class Solution {
     fun solution(my_string: String, index_list: IntArray): String {
-        val stringList = my_string.toList()
-        val sb = StringBuilder()
-        index_list.forEach {
-            sb.append(stringList[it])
-        }
-
-        return sb.toString()
+       return index_list.map { my_string[it] }.joinToString("")
     }
 }
