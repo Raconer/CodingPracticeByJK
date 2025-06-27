f21ff34a518621018b69c35a556267f6f2e32095
Dongho Kim <cha11enge@hanmail.net>
2025-06-27 16:24:05 +0900

    [level 0] Title: 카운트 다운, Time: 14.28 ms, Memory: 65 MB -BaekjoonHub

---
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.kt
@@ -0,0 +1,8 @@
+class Solution {
+    fun solution(start_num: Int, end_num: Int): IntArray {
+        return (start_num downTo end_num step 1).map {
+            
+            it
+        }.toIntArray()
+    }
+}
