e9c6580b8f6079a0b57882397eb8acf2ff927e9b
Dongho Kim <cha11enge@hanmail.net>
2025-06-28 15:28:32 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 16.19 ms, Memory: 64.1 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/README.md
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 택배 상자 꺼내기 - 389478 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/389478) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/389478#) 
 
 ### 성능 요약
 
-메모리: 64.1 MB, 시간: 15.23 ms
+메모리: 64.1 MB, 시간: 16.19 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 28일 15:26:14
+2025년 06월 28일 15:28:30
 
 ### 문제 설명
 
--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.kt
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.kt
@@ -5,15 +5,9 @@ class Solution {
         val row = n/w + if(n%w == 0) 0 else 1
         val findRow = num/w + if(num%w == 0) 0 else 1
         val tempCol = w*findRow - num
-        println("tempCol = $tempCol")
         val findCol = (if(findRow%2 == 0) w-tempCol  else  tempCol + 1) 
-
         var temp = num
-        println("현재 층수 : $findRow / $findCol")
-        println("홀수 Col : ${num/w}")
-
-        if(row == findRow) return 1
-
+        
         for(i in findRow..row){
             val value = if(i%2 == 0){
                 print("짝[${  w - findCol}] -> ")
