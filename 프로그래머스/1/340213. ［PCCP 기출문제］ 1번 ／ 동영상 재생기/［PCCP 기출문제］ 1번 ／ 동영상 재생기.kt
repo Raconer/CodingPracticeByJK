80a37f0711cc7cdec7ac17068743335139a31d65
Dongho Kim <cha11enge@hanmail.net>
2025-06-28 23:01:53 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 16.42 ms, Memory: 63.7 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.kt
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.kt
@@ -1,62 +1,30 @@
 class Solution {
-    
-    private var videoLenTime = 0
-    private var opStartTime = 0
-    private var opEndTime = 0
-    
-     fun solution(video_len: String, pos: String, op_start: String, op_end: String, commands: Array<String>): String {
-
-     
-        videoLenTime = changeTime(video_len)
-        opStartTime = changeTime(op_start)
-        opEndTime = changeTime(op_end)
-
-        var posTime = changeTime(pos, true)
-
-        commands.forEach {
-            when(it){
-                "next" -> {posTime = this.plusTime(posTime, 10)}
-                "prev" -> {posTime = this.plusTime(posTime, -10)}
+    fun solution(video_len: String, pos: String, op_start: String, op_end: String, commands: Array<String>): String {
+        val videoLenTime = changeSecond(video_len)
+        var posTime = changeSecond(pos)
+        val opStartTime = changeSecond(op_start)
+        val opEndTime = changeSecond(op_end)
+        
+        if(posTime in opStartTime..opEndTime) posTime = opEndTime
+        
+        commands.forEach{
+            posTime = when(it){
+                "next" -> (posTime + 10).coerceAtMost(videoLenTime)
+                "prev" -> (posTime - 10).coerceAtLeast(0)
+                else -> posTime
             }
+            
+             if(posTime in opStartTime..opEndTime) posTime = opEndTime
         }
-        val minute = posTime / 100
-        val second = posTime % 100
-        return "${if(minute < 10) "0$minute" else minute}:${if(second < 10) "0$second" else second}"
+        
+       
+        return "%02d:%02d".format(posTime/60, posTime%60)
     }
-
-    private fun changeTime(timeStr:String, isPosTime: Boolean = false):Int{
-        val timeList = timeStr.split(":")
-        val time =  timeList[0].toInt()*100 + timeList[1].toInt()
-          return if(isPosTime && time in opStartTime..opEndTime) opEndTime else time
+    
+    private fun changeSecond(timeStr:String):Int{
+        val time = timeStr.split(":")
+        val minute = time[0].toInt() * 60
+        val second = time[1].toInt()
+        return minute + second
     }
-
-    private fun plusTime(time:Int, addTime:Int) :Int{
-        var minute = time/100
-        var second = time%100
-
-        second += addTime
-
-        if(60 <= second) {
-            minute += 1
-            second -= 60
-        } else if(second < 0){
-            minute -= 1
-            second = 60 + second
-            if((minute*100) + second < 10){
-                minute = 0
-                second = 0
-            }
-        }
-
-        var time = (minute*100) + second
-
-       if( videoLenTime  < time){
-           time = videoLenTime
-       }
-
-       return if(time in opStartTime..opEndTime ){
-           opEndTime
-       } else time
-    }
-
 }
