798524d2f70534a8bd22b399756a07a54fae4955
kimdongho <cha11enge@hanmail.net>
2025-06-28 15:34:00 +0900

    [PROGRAMMERS] 정답 주석 수정

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.kt
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.kt
@@ -1,30 +1,26 @@
 class Solution {
     fun solution(n: Int, w: Int, num: Int): Int {
-        var answer: Int = 1
+        fun solution(n: Int, w: Int, num: Int): Int {
+            var answer: Int = 1
 
-        val row = n/w + if(n%w == 0) 0 else 1
-        val findRow = num/w + if(num%w == 0) 0 else 1
-        val tempCol = w*findRow - num
-        val findCol = (if(findRow%2 == 0) w-tempCol  else  tempCol + 1) 
-        var temp = num
-        
-        for(i in findRow..row){
-            val value = if(i%2 == 0){
-                print("짝[${  w - findCol}] -> ")
-                w - findCol
-            }else {
-                print("홀[${findCol - 1}]-> ")
-                findCol - 1
+            val row = n/w + if(n%w == 0) 0 else 1 // 총 층 수
+            val findRow = num/w + if(num%w == 0) 0 else 1 // 찾을는 번호 층수
+            val tempCol = w*findRow - num // 박스 위치
+            val findCol = (if(findRow%2 == 0) w-tempCol  else  tempCol + 1)  // 오른쪽 기준 박스 위치
+            var a = num
 
+            for(i in findRow..row){ // 박스 층수 -> 전체 층수
+                val value = if(i%2 == 0){ // 짝수, 홀수 등차 수열
+                    w - findCol
+                }else {
+                    findCol - 1
+                }
+                a += ( value*2 )+1
+
+                if(a <= n) answer += 1 // 등차 수열이 전체 박스 갯수보다 작으면 +1
             }
-            println("$temp + ${(value*2) + 1}")
-            temp += ( value*2 )+1
 
-            if(temp <= n) answer += 1
+            return answer
         }
-
-        println("답 : $answer")
-        println("=========")
-        return answer
     }
 }
