9cb9873657c48c7166aa5556d1a49d16a08b6d64
Dongho Kim <cha11enge@hanmail.net>
2025-07-10 14:04:12 +0900

    [level 3] Title: 이중우선순위큐, Time: 62.10 ms, Memory: 98.1 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.kt
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.kt
@@ -1,56 +1,33 @@
-
-import java.util.LinkedList
-import java.util.Queue
-
+import java.util.*
 
 class Solution {
     fun solution(operations: Array<String>): IntArray {
-        val queue:Queue<Int> = LinkedList<Int>()
+          val minQ = PriorityQueue<Int>()                            // 오름차순 (최솟값)
+    val maxQ = PriorityQueue<Int>(compareByDescending { it })   // 내림차순 (최댓값)
 
-        operations.forEach {
-            this.cmdAction(it, queue)
-        }
-
-        return if(queue.isEmpty()) intArrayOf(0,0)
-        else {
-            val max = this.findMinMax(queue, true)
-            val min = this.findMinMax(queue, false)
-            intArrayOf(max, min)
-        }
-    }
-    
-      fun cmdAction(cmd:String, queue: Queue<Int>){
-
-       if(cmd.startsWith("I ")){ // 값 넣기
-           val number = cmd.split(" ")[1].toInt()
-           queue.offer(number)
-       }else if(cmd == "D 1"){ // 최대값 삭제
-            queue.remove(this.findMinMax(queue, true))
-       }else if(cmd == "D -1"){ // 최소값 삭제
-           queue.remove(this.findMinMax(queue, false))
-       }
-    }
-
-    fun findMinMax(queue: Queue<Int>, isMinMax: Boolean):Int{
-        var number:Int? = null
-        val tempQueue:Queue<Int> = LinkedList(queue)
-
-        while(tempQueue.isNotEmpty()){
-            val tempNumber = tempQueue.poll()
-
-            number?.let {
-                if(isMinMax && it < tempNumber){
-                    number = tempNumber
-                } else if(!isMinMax && it > tempNumber){
-                    number = tempNumber
-                }
-            }?:run {
-               number = tempNumber
+    operations.forEach { op ->
+        when {
+            op.startsWith("I ") -> {
+                val num = op.substring(2).toInt()
+                minQ.add(num)
+                maxQ.add(num)
             }
-
-
+            op == "D 1" -> {
+                if (maxQ.isNotEmpty()) {
+                    val max = maxQ.poll()
+                    minQ.remove(max)
+                }
+            }
+            op == "D -1" -> {
+                if (minQ.isNotEmpty()) {
+                    val min = minQ.poll()
+                    maxQ.remove(min)
+                }
+            }
         }
+    }
 
-        return number?:0
+    return if (minQ.isEmpty()) intArrayOf(0, 0)
+    else intArrayOf(maxQ.peek(), minQ.peek())
     }
 }
